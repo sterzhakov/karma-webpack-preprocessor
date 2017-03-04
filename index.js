de0e8e79a331e50459e6b0b536d44740d6f30b95
Igor <sterjakov@icloud.com>
2017-03-04 17:23:26 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const MemoryFS = require('memory-fs')
+const webpack = require('webpack')
+
+const getWebpackConfig = (file) => {
+  const webpackConfigPath = path.resolve(file)
+  const webpackConfig = require(webpackConfigPath)
+  return Object.assign({}, webpackConfig)
+}
+
+// https://webpack.js.org/api/node/#compiling-to-memory
+const compileWebpack = (config, file, done) => {
+
+  const parsedPath = path.parse(file)
+
+  config.entry = file
+  config.output = {
+    filename: parsedPath.base,
+    path: parsedPath.dir,
+  }
+
+  const fs = new MemoryFS()
+  const compiler = webpack(config)
+
+  compiler.outputFileSystem = fs
+  compiler.run((err) => {
+    if (err) done(err)
+    const contentBuffer = fs.readFileSync(file)
+    done(null, contentBuffer)
+  })
+}
+
+const createWebpackPreprocessor = (args, config, logger) => {
+
+  const log = logger.create('preprocessor.webpack')
+
+  return (content, file, done) => {
+    log.debug('Processing "%s".', file.path)
+    const webpackConfig = getWebpackConfig(config.configPath)
+    const result = compileWebpack(
+      webpackConfig,
+      file.path,
+      (err, result) => done(null, result)
+    )
+  }
+  
+}
+
+createWebpackPreprocessor.$inject = [
+  'args',
+  'config.webpackPreprocessor',
+  'logger'
+]
+
+module.exports = {
+  'preprocessor:webpack': ['factory', createWebpackPreprocessor]
+}
